35833ffd3a0c1fb2841a7fe72b0669bf05d575b1
Dabaz420 <73653156+Dabaz420@users.noreply.github.com>
2020-12-21 14:07:41 +0100

    Create Exo.js

---
+++ Exo.js
@@ -0,0 +1,37 @@
+function mastermind(){
+    let tablePossible = ["A", "B", "C", "D", "E"];
+    let devinette = [];
+    for(let i = 0; i < 3; i++){
+        devinette[i] = tablePossible[Math.floor((Math.random() * 5) + 0)];
+    }
+    console.log(devinette);
+    var reponse = prompt("Choisi les 3 bonnes lettres (choix entre A,B,C,D et E)");
+    let correction = [];
+    for(let y = 0; y <= 2; y++){
+        for(let i = 0; i < 3; i++){
+            if(devinette.includes(reponse[i])){
+                if(reponse[i] == devinette[i]){
+                    correction[i] = 'oui';
+                } 
+                else{
+                    correction[i] = 'presque';
+                }
+            } 
+            else if(!devinette.includes(reponse[i])){
+                correction[i] = 'non';
+            }
+        }
+        if(correction[0] == 'oui' && correction[1] == 'oui' && correction[2] == 'oui'){
+            alert('OK');
+            return;
+        }
+        else if(y == 2){
+            alert('KO');
+            return;
+        }
+        reponse = prompt(reponse + '\r\n' + correction[0] + ',' + correction[1] + ',' + correction[2]);
+    }
+    console.log(correction);
+}
+
+mastermind();
